1234747cae1de9530048ac46d9321780037a0175
pksaini <pankaj.k.saini@oracle.com>
2023-02-28 17:22:09 +0530

    generics

---
+++ bounded generics/generics.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+import javax.print.event.PrintEvent;
+
+public class generics{
+    public static void main(String[] args) {
+        String[] strs = {"abc", "bcd", "def"};
+        Integer[] nums = {1, 2, 3, 4};
+
+        System.out.println(convertIntoList(strs));
+        System.out.println(convertIntoList(nums));
+    }
+
+    public static <T> List<T> convertIntoList(T[] input){
+        return Arrays.asList(input);
+    }
+}
